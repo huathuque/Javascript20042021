77dc95d4359b74310f740e844742768b91533054
Que Hua <huathuque@gmail.com>
2021-04-22 18:32:26 +0700

    thay doi luu file

---
+++ buoi1.js
@@ -0,0 +1,14 @@
+// 1: tạo thư mục qly source code .git
+// git init
+// 2: kiểm tra sự thay đổi của folder
+// git status
+// 3: lưu trữ sự thay đổi
+// git add 'tenfile' (lưu sự thay đổi 1 file)
+// git add '.' (lưu tất cả sự thay đổi)
+// 4: Commit vào repository
+// git commit -m "lưu file buoi1"
+
+// If you're not ever use Git, config following instructions
+// git config --global user.email "huathuque@gmail.com"
+// git config --global user.name "Que Hua"
+
