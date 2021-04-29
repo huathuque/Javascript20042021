a02003489d67d792e2895738a0226ffaa9ff1728
Que Hua <huathuque@gmail.com>
2021-04-29 18:37:46 +0700

    reduce

--- buoi1.js
+++ buoi1.js
@@ -299,3 +299,92 @@
 // tham số index không sử dụng có thể xóa khỏi hàm map cũng được
 // map thay thế thằng phía trên, viết nhanh hơn.
 // Quản lý tập trung
+
+//15 - Filter
+// dữ liệu mảng có thể gọi filter
+// không cần khởi tạo filter
+// không chỉnh sửa phần tử
+
+// const arrNames = ['An','Bình','Chương']
+// const newArrNames = arrNames.filter(function(name){
+//     if (name.length >= 3){
+//         return true
+//     }
+//     return false
+// })
+// console.log(newArrNames)
+
+// // Ví dụ
+// const arrPersons = [
+//     {id : 1 , name : 'Tèo' , age : 20},
+//     {id : 2 , name : 'Tủn' , age : 25},
+//     {id : 3 , name : 'Hoa' , age : 22},
+//     {id : 4 , name : 'Tuấn' , age : 21},
+//     {id : 5 , name : 'Lan' , age : 26},
+// ]
+
+// const filterArrPersons = arrPersons.filter(function(person){
+//     if (person.age < 25){
+//         return true;
+//     }
+//     return false;
+// }).map(function(object){
+//     return object.name
+// })
+
+// console.log(filterArrPersons)
+
+// // yêu cầu: in ra mảng person dưới 25t, chỉ lấy name.
+
+//16 - Hàm reduce - gộp
+
+// tham số thứ 2: truyền giá trị ban đầu
+// số thì để 0
+// object hoặc array thì để rỗng
+// accumulator: biến tích lũy
+// const arrNums = [1,2,3,4,5]
+
+// const newArrNums = arrNums.reduce(function(accumulator, currentValue){
+//     return accumulator + currentValue
+//     // biến tích lũy (dựa vào cái này để chạy) + giá trị hiện tại (dùng để check điều kiện)
+//     // Lần 1:
+//     // accumulator : 0 (Value) - giá trị khởi tạo ban đầu
+//     // currentValue : 1 (Value) - giá trị đầu tiên
+//     // return 5
+//     // Lần 2:
+//     // accumulator : 5 (Value)
+//     // currentValue : 2 (Value)
+//     // return 7
+//     // Lần 3:
+//     // accumulator : 7 (Value)
+//     // currentValue : 3 (Value)
+// },0)
+
+// console.log(newArrNums)
+
+// // Ví dụ
+// const arrPersons = [
+//     {id : 1 , name : 'Tèo' , age : 20},
+//     {id : 2 , name : 'Tủn' , age : 25},
+//     {id : 3 , name : 'Hoa' , age : 22},
+//     {id : 4 , name : 'Tuấn' , age : 21},
+//     {id : 5 , name : 'Lan' , age : 26},
+// ]
+
+// const newArrPersons = arrPersons.reduce(function(accumulator, currentValue){
+//     if (currentValue.age < 25){
+//         accumulator.push(currentValue.name);
+//     }
+//     return accumulator
+// },[])
+// console.log(newArrPersons)
+
+// // yêu cầu: in ra mảng person dưới 25t, chỉ lấy name.
+
+//Bài tập khó:
+
+function countOccurrences(arr) {
+
+}
+countOccurrences(["a", "b", "c", "b", "a", "a"]);
+// {a : 3, b : 2, c : 1}
